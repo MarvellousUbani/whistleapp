2dac205c721a0b1c631f6a9fa0310913e9fe4281
Marvellous Michaels <marvellousubani@gmail.com>
2020-05-07 00:37:29 +0100

    filtered out unused restful routes on routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,16 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get 'help', to: 'static_pages#help'
 
-  resources :likes
-  resources :followings
-  resources :opinions
+  resources :likes, only: [:create, :destroy]
+  resources :followings, only: [:create, :destroy]
+  resources :opinions, only: [:index, :create, :destroy]
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'opinions#index'
   get '/signup/' ,to: 'users#new' 
   get '/login', to: 'sessions#new'
 	post 'login', to: 'sessions#create'
 	delete '/logout', to: 'sessions#destroy'
-
-	resources :users
+	
 end
